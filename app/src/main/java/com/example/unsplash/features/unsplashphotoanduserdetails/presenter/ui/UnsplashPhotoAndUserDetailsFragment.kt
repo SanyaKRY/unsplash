70d38373d652925ea2686de0e22098c3d73841aa
Aliaksandr Kreyer <aliaksandr_kreyer@epam.com>
2023-02-20 13:30:17 +0300

    Updat UnsplashPhotoAndUserDetailsFragment view and link navigation

--- app/src/main/java/com/example/unsplash/features/unsplashphotoanduserdetails/presenter/ui/UnsplashPhotoAndUserDetailsFragment.kt
+++ app/src/main/java/com/example/unsplash/features/unsplashphotoanduserdetails/presenter/ui/UnsplashPhotoAndUserDetailsFragment.kt
@@ -1,11 +1,22 @@
 package com.example.unsplash.features.unsplashphotoanduserdetails.presenter.ui
 
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.Typeface
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.example.unsplash.R
@@ -30,14 +41,50 @@ class UnsplashPhotoAndUserDetailsFragment : Fragment() {
         args.apply {
             val unsplashPhoto: UnsplashPhotoAndUserDetailsUi = UiToUnsplashPhotoAndUserUiMapper.map(unsplashPhoto.unsplashPhotoDetailsBundleModel)
 
-            binding.unsplashPhotoId.text = getString(R.string.unsplash_photo_id, unsplashPhoto.id)
-            binding.unsplashPhotoLikes.text = getString(R.string.unsplash_photo_likes, unsplashPhoto.likes.likes)
-            binding.userUsername.text = getString(R.string.user_username, unsplashPhoto.user.username)
-            binding.userLinksHtml.text = getString(R.string.user_links_html, unsplashPhoto.user.links.html)
+            binding.unsplashPhotoId.text = setTextView(binding.unsplashPhotoId.text, unsplashPhoto.id)
+            binding.unsplashPhotoLikes.text = setTextView(binding.unsplashPhotoLikes.text, unsplashPhoto.likes.likes.toString())
+            binding.userUsername.text = setTextView(binding.userUsername.text, unsplashPhoto.user.username)
+
+            binding.userLinksHtml.text = setClickableLink(binding.userLinksHtml.text, unsplashPhoto.user.links.html)
+            binding.userLinksHtml.movementMethod = LinkMovementMethod.getInstance()
+
             loadImage(binding.unsplashPhotoImageView, unsplashPhoto.urlsRegular)
         }
 
         return view
+    }
+
+    private fun setClickableLink(textTemplate: CharSequence, unsplashPhotoDetail: String): SpannableStringBuilder {
+        val ss = SpannableStringBuilder(textTemplate)
+        val boldSpan = StyleSpan(Typeface.BOLD)
+        val colorSpan = ForegroundColorSpan(Color.RED)
+        ss.setSpan(boldSpan, 0, textTemplate.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        ss.setSpan(colorSpan, 0, textTemplate.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        ss.append(" ").append("link")
+
+        val clickableSpan: ClickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                Log.d("PetProject", "url: ${unsplashPhotoDetail}")
+                val url = unsplashPhotoDetail
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse(url)
+                startActivity(intent)
+
+            }
+        }
+        ss.setSpan(clickableSpan, textTemplate.length + 1, ss.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        return ss
+    }
+
+    private fun setTextView(textTemplate: CharSequence, unsplashPhotoDetail: String): SpannableStringBuilder {
+        val ss = SpannableStringBuilder(textTemplate)
+        val boldSpan = StyleSpan(Typeface.BOLD)
+        val colorSpan = ForegroundColorSpan(Color.RED)
+        ss.setSpan(boldSpan, 0, textTemplate.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        ss.setSpan(colorSpan, 0, textTemplate.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        return ss.append(" ").append(unsplashPhotoDetail)
     }
 
     private fun loadImage(imageView: AppCompatImageView, imageUri: String) {
